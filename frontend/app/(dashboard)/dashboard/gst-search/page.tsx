ccb3dd6da258e1b69be2c552d7487cf7d09f21a6
kartikeyevanik <kartikey.evanik@gmail.com>
2025-09-15 18:11:42 +0530

    protected gstsearch route

--- frontend/app/(dashboard)/dashboard/gst-search/page.tsx
+++ frontend/app/(dashboard)/dashboard/gst-search/page.tsx
@@ -1,9 +1,12 @@
+import ProtectedRoute from "@/components/auth/ProtectedRoute";
 import GstSearchForm from "@/components/forms/gst-search-form";
 
 export default async function GstSearchPage() {
     return (
-        <div className="md:ml-[2rem]">
-            <GstSearchForm />
-        </div>
+        <ProtectedRoute>
+            <div className="md:ml-[2rem]">
+                <GstSearchForm />
+            </div>
+        </ProtectedRoute>
     )
 }
