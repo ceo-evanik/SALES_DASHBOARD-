83227a435e9b2edb0981a5e8eadede70b3361aaa
kartikeyevanik <kartikey.evanik@gmail.com>
2025-09-18 15:47:04 +0530

    updated protected route  with admin and user roles

--- frontend/app/(dashboard)/dashboard/page.tsx
+++ frontend/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,11 @@
-import ProtectedRoute from '@/components/auth/ProtectedRoute'; 
+import ProtectedRoute from '@/components/auth/ProtectedRoute';
 import DashboardPageComponent from '@/components/dash';
 import BillingPage from '@/components/billing';
 import ReportPage from '@/components/reports';
 
 export default function MainPage() {   // <-- rename local function
   return (
-    <ProtectedRoute>
+    <ProtectedRoute allowedRoles={["admin", "user"]}>
       <DashboardPageComponent />
       <BillingPage />
       <ReportPage />
--- frontend/components/auth/ProtectedRoute.tsx
+++ frontend/components/auth/ProtectedRoute.tsx
@@ -2,25 +2,36 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useUser } from '@/context/UserProvider';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
+  allowedRoles?: string[];
 }
 
-export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+export default function ProtectedRoute({ children, allowedRoles = [] }: ProtectedRouteProps) {
   const router = useRouter();
+  const { user, loading: userLoading } = useUser();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    if (!token) {
-      router.replace('/signin'); // redirect to login if not logged in
-    } else {
-      setIsLoading(false); // token exists, show dashboard
-    }
-  }, [router]);
 
-  if (isLoading) return <p className="text-center mt-20">Loading...</p>;
+    if (!token) {
+      router.replace('/signin');
+      return;
+    }
+
+    if (!userLoading) {
+      if (allowedRoles.length > 0 && !allowedRoles.includes(user?.userType || '')) {
+        router.replace('/unauthorized');
+        return;
+      }
+      setIsLoading(false);
+    }
+  }, [router, user, userLoading, allowedRoles]);
+
+  if (isLoading || userLoading) return <p className="text-center mt-20">Loading...</p>;
 
   return <>{children}</>;
 }
