b8674f3ff1d22186f57a7eb4481e0ed0544566be
Sujal <sujalevanik.com>
2025-09-18 14:35:26 +0530

    update in usertype

--- Backend/validators/user.validator.js
+++ Backend/validators/user.validator.js
@@ -6,7 +6,7 @@ export const createUserValidation = [
   body("name").notEmpty().withMessage("Name is required"),
   body("email").isEmail().withMessage("Valid email required"),
   body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 chars"),
-  body("userType").isIn(["admin","support","sales"]).withMessage("userType must be admin/support/sales"),
+  body("userType").isIn(["admin","user"]).withMessage("userType must be admin/user"),
 
   // If sales then require sales-specific fields
   body("salespersonId")
@@ -32,9 +32,8 @@ export const updateUserValidation = [
   body("name").optional().notEmpty().withMessage("Name cannot be empty"),
   body("email").optional().isEmail().withMessage("Valid email required"),
   body("password").optional().isLength({ min: 6 }).withMessage("Password must be at least 6 chars"),
-  body("userType").optional().isIn(["admin","support","sales"]).withMessage("userType must be admin/support/sales"),
+  body("userType").optional().isIn(["admin","user"]).withMessage("userType must be admin/user"),
 
-  // If sales then require sales-specific fields when provided
   body("salespersonId")
     .if(body("userType").equals("sales"))
     .optional()
